b4ac87f46207bc3e74d7307cd9e08210ccf1a58f
Arda Yildirim <ardayildirim05@icloud.com>
2024-02-07 18:03:24 +0100

    Implement custom checkbox component

---
+++ src/components/form/Checkbox.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { ReactNode, useState } from "react";
+import { FaCheck } from "react-icons/fa6";
+
+interface Props {
+  id: string;
+  children: string | ReactNode;
+  className?: string;
+}
+
+const Checkbox: React.FC<Props> = ({ id, children, className = "" }: Props) => {
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+
+  const toggleChecked = () => {
+    console.log("check");
+    setIsChecked(!isChecked);
+  };
+
+  const checkboxClassName = `checkbox ${isChecked ? "checked" : ""}`;
+
+  const getTextContent = () =>
+    React.Children.map(children, (child) => {
+      if (
+        React.isValidElement(child) &&
+        child.props.className === "checkbox-clickable" &&
+        child.type === "span"
+      ) {
+        return React.cloneElement(child, {
+          onClick: toggleChecked,
+        } as React.HTMLAttributes<HTMLSpanElement>);
+      }
+      return child;
+    });
+
+  const getClickProp = () => {
+    return typeof children === "string" ? { onClick: toggleChecked } : {};
+  };
+
+  console.log(getClickProp());
+
+  return (
+    <div className={`checkbox-container ${className}`} {...getClickProp()}>
+      <div className={checkboxClassName} id={id}>
+        <FaCheck className="checkbox-check" size={17} color="#f5f5f5" />
+      </div>
+      <p style={{ userSelect: "none" }} className="checkbox-label">
+        {typeof children === "string" ? children : getTextContent()}
+      </p>
+    </div>
+  );
+};
+
+export default Checkbox;
