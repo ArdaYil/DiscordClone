51e41aef8f93c8f3ccb538189618bc4252b14c74
Arda Yildirim <ardayildirim05@icloud.com>
2024-02-11 20:38:35 +0100

    Make input component send changes to consumer

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -1,23 +1,47 @@
 import { ReactNode } from "react";
+import { FormChangeEvent } from "./Form";
+import InputLabel from "./InputLabel";
 
 type InputType = "TEXT" | "PASSWORD";
 
 interface Props {
   title: string;
+  onChange: (e: FormChangeEvent) => void;
+  name: string;
   required?: boolean;
+  error?: string;
   type?: InputType;
   icon?: ReactNode;
+  className?: string;
 }
 
-const Input = ({ title, icon, required = true, type = "TEXT" }: Props) => {
+const Input = ({
+  title,
+  onChange,
+  name,
+  icon,
+  className = "",
+  required = true,
+  error,
+  type = "TEXT",
+}: Props) => {
+  const inputFieldClassName = className ? className + "-input-field" : "";
+
   return (
-    <label className="text-input">
-      <p className="input-title">
-        {title}
-        {required && <span className="input-required">*</span>}
-      </p>
+    <label className={`text-input ${className}`}>
+      <InputLabel
+        title={title}
+        className={className}
+        required={required}
+        error={error}
+      ></InputLabel>
       <div className="input-field-container">
-        <input type={type} className="input-field" />
+        <input
+          name={name}
+          onChange={onChange}
+          type={type}
+          className={`input-field ${inputFieldClassName}`}
+        />
         {icon}
       </div>
     </label>
